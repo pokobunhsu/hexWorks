023eeb693a54889125bed43e44fa7205551bba13
poko <minshanzen@gmail.com>
2022-03-02 16:31:20 +0800

    first commit.
    初版總是比較差一些，我會繼續改的😂

---
+++ work1/script.js
@@ -0,0 +1,6 @@
+let send = () => {
+    let message = document.querySelector('#message')
+    let name = document.querySelector('#name')
+    let phone = document.querySelector('#phone')
+    let email = document.querySelector('#email')
+}
